285833696e5bd33e1b2ba9d6f70787b705a1d470
Sebastian Romero <sebrom05@gmail.com>
2025-11-13 15:56:13 -0500

    Agregar las credenciales del servidor de base de datos

--- includes/config/database.php
+++ includes/config/database.php
@@ -24,14 +24,20 @@ function conectarDB()
 {
     // Cargar las variables de entorno
     cargarEnv(dirname(__DIR__, 2) . '/.env');
+    error_log("🔍 DB_HOST: " . ($_ENV['DB_HOST'] ?? 'NO'));
+    error_log("🔍 DB_NAME: " . ($_ENV['DB_NAME'] ?? 'NO'));
+    error_log("🔍 DB_USER: " . ($_ENV['DB_USER'] ?? 'NO'));
+    error_log("🔍 DB_PORT: " . ($_ENV['DB_PORT'] ?? 'NO'));
+    error_log("🔍 DB_PASS: " . ($_ENV['DB_PASS'] ?? 'NO'));
 
     $servidor   = $_ENV['DB_HOST'];
     $usuario    = $_ENV['DB_USER'];
     $contrasena = $_ENV['DB_PASS'];
     $dbname     = $_ENV['DB_NAME'];
+    $puerto = $_ENV['DB_PORT'];
 
     try {
-        $conexion = new PDO("pgsql:host=$servidor;dbname=$dbname", $usuario, $contrasena);
+        $conexion = new PDO("pgsql:host=$servidor;port=$puerto;dbname=$dbname", $usuario, $contrasena);
         $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $conexion->exec("SET search_path TO public;");
         return $conexion;
