9ddad912e392a24202ecd37da2101a13bafcecea
Sebastian Romero <sebrom05@gmail.com>
2025-11-14 11:15:27 -0500

    Probando Router con el cambio 2

--- public/index.php
+++ public/index.php
@@ -9,7 +9,7 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 // Ruta al archivo Router.php
-$routerPath = dirname(__DIR__) . 'Router.php';
+$routerPath = __DIR__ . '/Router.php';
 
 // Verifica que exista y lo carga
 if (file_exists($routerPath)) {
