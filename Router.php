546a23316ca1c6bf0b3f9f304a2eff246794ce1d
Sebastian Romero <sebrom05@gmail.com>
2025-11-17 11:33:39 -0500

    Mejorando el Router

--- Router.php
+++ Router.php
@@ -14,8 +14,7 @@ require_once BASE_PATH . '/includes/cors.php';
 
 // Detectar método y ruta
 $method = $_SERVER['REQUEST_METHOD'];
-$uri = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
-
+$requestUri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 // 🔹 Ya NO elimines /public, porque estás corriendo desde Router.php
 $uri = $requestUri;
